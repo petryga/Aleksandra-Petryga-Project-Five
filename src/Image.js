48a4a9a9d99052d7eb59bd903052a5da5fbdbd45
Aleksandra Petryga <apetryga@gmail.com>
2020-12-03 16:41:56 -0500

    FINAL - fixed bleeding :whale2::dash:

--- src/Image.js
+++ src/Image.js
@@ -1,6 +1,6 @@
 const Image = ({ url, alt, author }) => {
     return (
-        <div className="image-container flex column">
+        <div className="image-container flex column wrapper">
             <img src={url} alt={alt ? alt : 'travel picture'} />
             <p>Image by {author}</p>
         </div>
